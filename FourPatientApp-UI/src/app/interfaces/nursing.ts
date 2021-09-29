c81092d75905d4b48f0bd40c22a93e0e24b81179
Jorge <jorgeyumiseba@gmail.com>
2021-09-29 18:00:09 -0400

    Css

---
+++ FourPatientApp-UI/src/app/interfaces/nursing.ts
@@ -0,0 +1,12 @@
+export interface Nursing{
+    id: number,
+    waitingrooms: number,
+    protocols: number,
+    separation: number,
+    safety: number,
+    covid1: boolean,
+    screening: number,
+    treatment: number,
+    averagec: number
+   
+}
