25a3bcf9592586ef29b783748e16aa8fb919e032
Jorge <jorgeyumiseba@gmail.com>
2021-09-29 15:13:16 -0400

    Superfix Scaffold

--- FourPatient.WebAPI/FourPatient.DataAccess/Entities/Hospital.cs
+++ FourPatient.WebAPI/FourPatient.DataAccess/Entities/Hospital.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 #nullable disable
 
-namespace FourPatient.WebAPI
+namespace FourPatient.DataAccess.Entities
 {
     public partial class Hospital
     {
--- FourPatient.WebAPI/FourPatient.DataAccess/ReviewRepo.cs
+++ FourPatient.WebAPI/FourPatient.DataAccess/ReviewRepo.cs
@@ -11,32 +11,32 @@ namespace FourPatient.DataAccess
    public  class ReviewRepo : IReview
     {
 
-        private readonly Patient4Context _context;
+        private readonly _4PatientContext _context;
 
-        public ReviewRepo(Patient4Context context)
+        public ReviewRepo(_4PatientContext context)
         {
             _context = context;
         }
 
-        public List<Review> ListReview()
-        {
-            return _context.Reviews
+        //public List<Review> ListReview()
+        //{
+        //    return _context.Reviews
 
-                .Select(n => new Review
-                {
-                    Id = n.Id,
-                    Patient = n.Patient, // foreign KEY
-                    Comfort = n.Comfort,
-                    DatePosted = n.DatePosted,
-                    Message = n.Message,
-                    Hospitalid = n.Hospitalid,
-                    AccommodationId = n.AccommodationId,
-                    NursingId = n.NursingId,
-                    CovidId = n.CovidId,
-                    CleanlinessId = n.CleanlinessId                 
-                })
-               .ToList();
-        }
+        //        .Select(n => new Review
+        //        {
+        //            Id = n.Id,
+        //            Patient = n.Patient, // foreign KEY
+        //            Comfort = n.Comfort,
+        //            DatePosted = n.DatePosted,
+        //            Message = n.Message,
+        //            Hospitalid = n.Hospitalid,
+        //            AccommodationId = n.AccommodationId,
+        //            NursingId = n.NursingId,
+        //            CovidId = n.CovidId,
+        //            CleanlinessId = n.CleanlinessId                 
+        //        })
+        //       .ToList();
+        //}
 
 
 
