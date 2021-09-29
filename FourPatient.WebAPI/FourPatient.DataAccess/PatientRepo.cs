7a3412a619ac866b2ae9d5f07ea6fb6d0340b775
Jorge <jorgeyumiseba@gmail.com>
2021-09-29 14:15:00 -0400

    Repos

--- FourPatient.WebAPI/FourPatient.DataAccess/PatientRepo.cs
+++ FourPatient.WebAPI/FourPatient.DataAccess/PatientRepo.cs
@@ -1,4 +1,5 @@
-﻿using FourPatient.Domain;
+﻿using FourPatient.DataAccess.Entities;
+using FourPatient.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,33 @@ namespace FourPatient.DataAccess
 {
     public class PatientRepo : IPatient
     {
+        private readonly Patient4Context _context;
+
+        public PatientRepo(Patient4Context context)
+        {
+            _context = context;
+        }
+
+        public List<Patient> ListPatient()
+        {
+            return _context.Patients
+
+                .Select(n => new Patient
+                {
+                    Id = n.Id,
+                    FirstName = n.FirstName,
+                    LastName = n.LastName,
+                    Password = n.Password,
+                    Street = n.Street,
+                    City = n.City,
+                    State = n.State,
+                    DoB = n.DoB,
+                    Email = n.Email,
+                    PhoneNumber = n.PhoneNumber,
+                    ZipCode = n.ZipCode
+                })
+               .ToList();
+        }
+
     }
 }
