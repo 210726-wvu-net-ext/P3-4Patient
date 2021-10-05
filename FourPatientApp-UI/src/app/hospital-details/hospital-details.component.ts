bac1984e89f24b472a1e715a6c2dccacf25727fa
Jorge <jorgeyumiseba@gmail.com>
2021-10-05 17:41:40 -0400

    Hospital Details

--- FourPatientApp-UI/src/app/hospital-details/hospital-details.component.ts
+++ FourPatientApp-UI/src/app/hospital-details/hospital-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HospitalService } from '../hospital.service';
+import { Hospital } from '../interfaces/hospital';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-hospital-details',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HospitalDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() hospital?: Hospital;
+
+  // hospital: Hospital[] | null = null;
+
+  constructor(private hospitalservice: HospitalService,  private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.GetDetails();
   }
+
+  GetDetails() {
+    const hospitalid = Number(this.route.snapshot.paramMap.get('id'));
+    console.log(hospitalid);
+    this.hospitalservice.GetHospitalbyId(hospitalid)
+    .subscribe(hospital => this.hospital = hospital);
+
+  }
+
+
 
 }
