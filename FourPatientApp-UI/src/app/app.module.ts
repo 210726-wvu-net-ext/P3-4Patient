fb5b4c07dbaf886e7e1edafe1bccf22d488ad90d
Jorge <jorgeyumiseba@gmail.com>
2021-10-06 10:00:59 -0400

    Cleanliness Survey

--- FourPatientApp-UI/src/app/app.module.ts
+++ FourPatientApp-UI/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateReviewComponent } from './create-review/create-review.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -55,9 +56,10 @@ import { ReviewViewComponent } from './review-view/review-view.component';
   imports: [
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule,
     BrowserModule,
-    FormsModule,
+
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
