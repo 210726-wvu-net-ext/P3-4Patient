d91c74b986b6a52bbfddb6565ad54a1a42937bf0
Jorge <jorgeyumiseba@gmail.com>
2021-10-12 02:41:49 -0400

    Stars implementation

--- FourPatientApp-UI/src/app/home/home.component.ts
+++ FourPatientApp-UI/src/app/home/home.component.ts
@@ -1,21 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user';
 import { HttpHeaders } from '@angular/common/http';
+import { NgbCarousel, NgbSlideEvent,NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
- 
+  // images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  // paused = false;
+  // unpauseOnArrow = false;
+  // pauseOnIndicator = false;
+  // pauseOnHover = true;
+  // pauseOnFocus = true;
   constructor(private http: HttpClient) { 
 
   }
 
   ngOnInit(): void {
   }
-  searchHospitals(searchString:string){
 
-  }
+  // @ViewChild('carousel', { static: true })
+  // carousel!: NgbCarousel;
+  // togglePaused() {
+  //   if (this.paused) {
+  //     this.carousel.cycle();
+  //   } else {
+  //     this.carousel.pause();
+  //   }
+  //   this.paused = !this.paused;
+  // }
+  // onSlide(slideEvent: NgbSlideEvent) {
+  //   if (this.unpauseOnArrow && slideEvent.paused &&
+  //     (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+  //     this.togglePaused();
+  //   }
+  //   if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+  //     this.togglePaused();
+  //   }
+  // }
+  
 }
--- FourPatientApp-UI/src/app/hospital-view/hospital-view.component.ts
+++ FourPatientApp-UI/src/app/hospital-view/hospital-view.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HospitalService } from '../hospital.service';
 import { Hospital } from '../interfaces/hospital';
 
 @Component({
   selector: 'app-hospital-view',
   templateUrl: './hospital-view.component.html',
-  styleUrls: ['./hospital-view.component.css']
+  styleUrls: ['./hospital-view.component.css'],
+
 })
 export class HospitalViewComponent implements OnInit {
+  currentRate= 4.7;
 
   constructor(private hospitalservice: HospitalService) { }
 
+
+  // @Input() starId;
 
   hospitals: Hospital[] = [];
   searchInput: string = "";
