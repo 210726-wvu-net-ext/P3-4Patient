9327e3117fded3cb874fa0a6f30787142ea848b8
Jorge <jorgeyumiseba@gmail.com>
2021-10-12 11:49:44 -0400

    Details and Name routerlink

--- FourPatientApp-UI/src/app/home/home.component.ts
+++ FourPatientApp-UI/src/app/home/home.component.ts
@@ -9,12 +9,12 @@ import { NgbCarousel, NgbSlideEvent,NgbSlideEventSource } from '@ng-bootstrap/ng
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  // images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  // paused = false;
-  // unpauseOnArrow = false;
-  // pauseOnIndicator = false;
-  // pauseOnHover = true;
-  // pauseOnFocus = true;
+  images = [62, 83, 466, 965, 982].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = false;
+  pauseOnFocus = true;
   constructor(private http: HttpClient) { 
 
   }
@@ -22,24 +22,24 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // @ViewChild('carousel', { static: true })
-  // carousel!: NgbCarousel;
-  // togglePaused() {
-  //   if (this.paused) {
-  //     this.carousel.cycle();
-  //   } else {
-  //     this.carousel.pause();
-  //   }
-  //   this.paused = !this.paused;
-  // }
-  // onSlide(slideEvent: NgbSlideEvent) {
-  //   if (this.unpauseOnArrow && slideEvent.paused &&
-  //     (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
-  //     this.togglePaused();
-  //   }
-  //   if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
-  //     this.togglePaused();
-  //   }
-  // }
+  @ViewChild('carousel', { static: true })
+  carousel!: NgbCarousel;
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
   
 }
